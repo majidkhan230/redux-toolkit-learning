6902b6ebd6d2e697c2ee158e92ff51f3e509e29e
MajidKh230 <khanmajid230@gmail.com>
2024-10-08 23:06:19 +0500

    added store

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-          <h1 className="text-3xl font-bold underline">
-      Hello world!
+          <h1 className="text-3xl font-bold underline text-center">
+     Learn about Redux Toolkit
     </h1>
     </div>
   )
