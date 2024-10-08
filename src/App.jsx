04c5d2a20bb2316be5162b6470e7d4f9528bf84b
MajidKh230 <khanmajid230@gmail.com>
2024-10-09 00:37:01 +0500

    work on tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import AddTodo from './components/AddTodo'
-import Todos from './components/Todos'
+import React from "react";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 const App = () => {
   return (
     <div>
-          <h1 className="text-3xl font-bold underline text-center">
-     Learn about Redux Toolkit
-    </h1>
-    <AddTodo/>
-    <Todos/>
+      <div className="flex mt-10 flex-col justify-center items-center">
+        <h1 className="text-3xl font-bold underline text-center">
+          Learn about Redux Toolkit
+        </h1>
+        <AddTodo />
+      </div>
+     <div className="w-[50vw] mx-auto"> <Todos /></div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
